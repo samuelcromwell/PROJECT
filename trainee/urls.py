b90d8fefa3a0772b2c2256b21d68d7c087d2cfb6
samuelcromwell <cromwellsamuel3@gmail.com>
2024-03-02 17:59:52 +0300

    fixed jazzmin logout page

--- trainee/urls.py
+++ trainee/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.base, name="base"),
     path('logout/',views.logoutUser, name="logout"),   
     path('profile/',views.profile, name="profile"),   
-
+    path('profile/edit',views.edit, name="edit"),   
+    path('profile/changepassword',views.change, name="change"),   
+    
 ]
--- trainee/views.py
+++ trainee/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from trainee.forms import EditProfileForm
 
 @login_required(login_url='traineelogin')
 def base(request):
@@ -17,4 +18,35 @@ def logoutUser(request):
      logout(request)
      return redirect('login')
 
-   
+def edit(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/trainee/profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form':form}
+        return render (request, 'trainee/edit.html', args)
+
+def change(request): 
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your password has been changed successfully')
+            return redirect('/trainee/profile')            
+        else:
+            messages.success(request, f'Your password has been changed successfully')
+            return redirect('/trainee/profile')
+    
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form':form}
+        return render (request, 'trainee/change.html', args)
+
+        
