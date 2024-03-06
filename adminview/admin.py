12ff704d8be68e18f09b28dd5ae46040a2213f7d
samuelcromwell <cromwellsamuel3@gmail.com>
2024-03-06 21:32:23 +0300

    trainee payment list fixed

--- adminview/admin.py
+++ adminview/admin.py
@@ -4,5 +4,12 @@ from .forms import PaymentForm
 
 class TraineePaymentAdmin(admin.ModelAdmin):
     form = PaymentForm
+    list_display = ('first_name', 'amount_paid', 'balance')
+
+    def first_name(self, obj):
+        return obj.trainee.first_name
+    
+    def balance(self, obj):
+        return obj.balance  # Assuming balance is already calculated and stored in the TraineePayment model
 
 admin.site.register(TraineePayment, TraineePaymentAdmin)
